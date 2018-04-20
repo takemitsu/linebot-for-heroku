722aac1b441fe879dfa2ce09131dbc7446af9aac
takemitsu <takemitsu@notespace.jp>
2018-04-20 15:59:01 +0900

    add TemplateSendMessage from linebot.models

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage,
 )
 
 app = Flask(__name__)
